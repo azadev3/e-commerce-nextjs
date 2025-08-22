3c395f990f386f8399cb27530284fcf7ae736a84
azadev3 <kodingo593@gmail.com>
2025-08-22 11:19:32 +0400

    .

---
+++ src/app/home/utils/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero: React.FC = () => {
+  return (
+    <section className="hero-wrapper">
+      <div className="hero">hero</div>
+    </section>
+  );
+};
+
+export default Hero;
